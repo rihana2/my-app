09ef46911ebaa636a8720b56673c4ae8ff918f4d
rihana2 <55271007+rihana2@users.noreply.github.com>
2019-10-14 22:08:36 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
 	String s1 = null;
 	String s1 = null;
 	String s1 = null;
-	String s2 = null;String s1 = null;String s3= null;String s4 = null;String s5 = null;String s6 = null;String s7 = null;
+	String s2 = null;String s1 = null;String s3= 4;String s4 = 7;String s5 = null;String s6 = null;String s7 = null;
 	
 	/*
 	 * @param i
